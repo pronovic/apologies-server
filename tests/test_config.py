0d7383d509e5ede4384d747c60ef723de78748f1
Kenneth Pronovici <ken.pronovici@objectpartners.com>
2020-05-12 14:51:30 -0500

    Add configuration for new websocket tasks

--- tests/test_config.py
+++ tests/test_config.py
@@ -16,14 +16,19 @@ def assert_defaults(c: SystemConfig) -> None:
     assert c.logfile_path is None
     assert c.server_host == "localhost"
     assert c.server_port == 8080
+    assert c.websocket_limit == 1000
     assert c.total_game_limit == 1000
     assert c.in_progress_game_limit == 25
     assert c.registered_player_limit == 100
+    assert c.websocket_idle_thresh_min == 2
+    assert c.websocket_inactive_thresh_min == 5
     assert c.player_idle_thresh_min == 15
     assert c.player_inactive_thresh_min == 30
     assert c.game_idle_thresh_min == 10
     assert c.game_inactive_thresh_min == 20
     assert c.game_retention_thresh_min == 2880
+    assert c.idle_websocket_check_period_sec == 120
+    assert c.idle_websocket_check_delay_sec == 300
     assert c.idle_player_check_period_sec == 120
     assert c.idle_player_check_delay_sec == 300
     assert c.idle_game_check_period_sec == 120
@@ -84,14 +89,19 @@ class TestFunctions:
         assert c.logfile_path == "/var/logs/apologies.log"
         assert c.server_host == "example.com"
         assert c.server_port == 8080
+        assert c.websocket_limit == 1000
         assert c.total_game_limit == 1000
         assert c.in_progress_game_limit == 25
         assert c.registered_player_limit == 100
+        assert c.websocket_idle_thresh_min == 2
+        assert c.websocket_inactive_thresh_min == 5
         assert c.player_idle_thresh_min == 15
         assert c.player_inactive_thresh_min == 30
         assert c.game_idle_thresh_min == 600  # taken from partial.rc
         assert c.game_inactive_thresh_min == 700  # taken from partial.rc
         assert c.game_retention_thresh_min == 2880
+        assert c.idle_websocket_check_period_sec == 120
+        assert c.idle_websocket_check_delay_sec == 300
         assert c.idle_player_check_period_sec == 120
         assert c.idle_player_check_delay_sec == 300
         assert c.idle_game_check_period_sec == 120
@@ -106,20 +116,25 @@ class TestFunctions:
         assert c.logfile_path == "/var/logs/apologies.log"
         assert c.server_host == "example.com"
         assert c.server_port == 10
-        assert c.total_game_limit == 20
-        assert c.in_progress_game_limit == 30
-        assert c.registered_player_limit == 40
-        assert c.player_idle_thresh_min == 50
-        assert c.player_inactive_thresh_min == 60
-        assert c.game_idle_thresh_min == 70
-        assert c.game_inactive_thresh_min == 80
-        assert c.game_retention_thresh_min == 90
-        assert c.idle_player_check_period_sec == 100
-        assert c.idle_player_check_delay_sec == 110
-        assert c.idle_game_check_period_sec == 120
-        assert c.idle_game_check_delay_sec == 130
-        assert c.obsolete_game_check_period_sec == 140
-        assert c.obsolete_game_check_delay_sec == 150
+        assert c.websocket_limit == 20
+        assert c.total_game_limit == 30
+        assert c.in_progress_game_limit == 40
+        assert c.registered_player_limit == 50
+        assert c.websocket_idle_thresh_min == 60
+        assert c.websocket_inactive_thresh_min == 70
+        assert c.player_idle_thresh_min == 80
+        assert c.player_inactive_thresh_min == 90
+        assert c.game_idle_thresh_min == 100
+        assert c.game_inactive_thresh_min == 110
+        assert c.game_retention_thresh_min == 120
+        assert c.idle_websocket_check_period_sec == 130
+        assert c.idle_websocket_check_delay_sec == 140
+        assert c.idle_player_check_period_sec == 150
+        assert c.idle_player_check_delay_sec == 160
+        assert c.idle_game_check_period_sec == 170
+        assert c.idle_game_check_delay_sec == 180
+        assert c.obsolete_game_check_period_sec == 190
+        assert c.obsolete_game_check_delay_sec == 200
 
     def test_load_config_overrides_partial(self):
         config_path = os.path.join(FIXTURE_DIR, "partial.rc")
@@ -134,14 +149,19 @@ class TestFunctions:
         assert c.logfile_path == "~/logs/apologies.log"  # taken from overrides
         assert c.server_host == "whatever.com"  # taken from overrides
         assert c.server_port == 8080
+        assert c.websocket_limit == 1000
         assert c.total_game_limit == 1000
         assert c.in_progress_game_limit == 25
         assert c.registered_player_limit == 100
+        assert c.websocket_idle_thresh_min == 2
+        assert c.websocket_inactive_thresh_min == 5
         assert c.player_idle_thresh_min == 15
         assert c.player_inactive_thresh_min == 30
         assert c.game_idle_thresh_min == 900  # taken from overrides
         assert c.game_inactive_thresh_min == 700  # taken from partial.rc
         assert c.game_retention_thresh_min == 2880
+        assert c.idle_websocket_check_period_sec == 120
+        assert c.idle_websocket_check_delay_sec == 300
         assert c.idle_player_check_period_sec == 120
         assert c.idle_player_check_delay_sec == 300
         assert c.idle_game_check_period_sec == 120
@@ -155,37 +175,47 @@ class TestFunctions:
             "logfile_path": "~/logs/apologies.log",
             "server_host": "whatever.com",
             "server_port": 100,
-            "total_game_limit": 200,
-            "in_progress_game_limit": 300,
-            "registered_player_limit": 400,
-            "player_idle_thresh_min": 500,
-            "player_inactive_thresh_min": 600,
-            "game_idle_thresh_min": 700,
-            "game_inactive_thresh_min": 800,
-            "game_retention_thresh_min": 900,
-            "idle_player_check_period_sec": 1000,
-            "idle_player_check_delay_sec": 1100,
-            "idle_game_check_period_sec": 1200,
-            "idle_game_check_delay_sec": 1300,
-            "obsolete_game_check_period_sec": 1400,
-            "obsolete_game_check_delay_sec": 1500,
+            "websocket_limit": 200,
+            "total_game_limit": 300,
+            "in_progress_game_limit": 400,
+            "registered_player_limit": 500,
+            "websocket_idle_thresh_min": 600,
+            "websocket_inactive_thresh_min": 700,
+            "player_idle_thresh_min": 800,
+            "player_inactive_thresh_min": 900,
+            "game_idle_thresh_min": 1000,
+            "game_inactive_thresh_min": 1100,
+            "game_retention_thresh_min": 1200,
+            "idle_websocket_check_period_sec": 1300,
+            "idle_websocket_check_delay_sec": 1400,
+            "idle_player_check_period_sec": 1500,
+            "idle_player_check_delay_sec": 1600,
+            "idle_game_check_period_sec": 1700,
+            "idle_game_check_delay_sec": 1800,
+            "obsolete_game_check_period_sec": 1900,
+            "obsolete_game_check_delay_sec": 2000,
         }
         load_config(config_path=config_path, overrides=overrides)
         c = config()
         assert c.logfile_path == "~/logs/apologies.log"
         assert c.server_host == "whatever.com"
         assert c.server_port == 100
-        assert c.total_game_limit == 200
-        assert c.in_progress_game_limit == 300
-        assert c.registered_player_limit == 400
-        assert c.player_idle_thresh_min == 500
-        assert c.player_inactive_thresh_min == 600
-        assert c.game_idle_thresh_min == 700
-        assert c.game_inactive_thresh_min == 800
-        assert c.game_retention_thresh_min == 900
-        assert c.idle_player_check_period_sec == 1000
-        assert c.idle_player_check_delay_sec == 1100
-        assert c.idle_game_check_period_sec == 1200
-        assert c.idle_game_check_delay_sec == 1300
-        assert c.obsolete_game_check_period_sec == 1400
-        assert c.obsolete_game_check_delay_sec == 1500
+        assert c.websocket_limit == 200
+        assert c.total_game_limit == 300
+        assert c.in_progress_game_limit == 400
+        assert c.registered_player_limit == 500
+        assert c.websocket_idle_thresh_min == 600
+        assert c.websocket_inactive_thresh_min == 700
+        assert c.player_idle_thresh_min == 800
+        assert c.player_inactive_thresh_min == 900
+        assert c.game_idle_thresh_min == 1000
+        assert c.game_inactive_thresh_min == 1100
+        assert c.game_retention_thresh_min == 1200
+        assert c.idle_websocket_check_period_sec == 1300
+        assert c.idle_websocket_check_delay_sec == 1400
+        assert c.idle_player_check_period_sec == 1500
+        assert c.idle_player_check_delay_sec == 1600
+        assert c.idle_game_check_period_sec == 1700
+        assert c.idle_game_check_delay_sec == 1800
+        assert c.obsolete_game_check_period_sec == 1900
+        assert c.obsolete_game_check_delay_sec == 2000
