a3fb682fd40e672b8fbe895e7eb66cd44d0d4ebb
Kenneth Pronovici <ken.pronovici@objectpartners.com>
2020-05-14 16:11:39 -0500

    Checkpoint on demo

--- src/apologiesserver/demo.py
+++ src/apologiesserver/demo.py
@@ -10,7 +10,7 @@ import asyncio
 import logging
 import signal
 from asyncio import AbstractEventLoop, CancelledError
-from typing import List, Optional
+from typing import List, Optional, cast
 
 import websockets
 from apologies.game import GameMode
@@ -52,10 +52,77 @@ async def _start_game(websocket: WebSocketClientProtocol, player_id: str) -> Non
     await send(websocket, request)
 
 
-def _handle_message(_websocket: WebSocketClientProtocol, _player_id: str, message: Message) -> Optional[Message]:
-    """Handle any message received from the connection."""
-    log.info("Ignored message with type %s", message.message.name)
+def _handle_game_joined(_player_id: str, message: Message) -> None:
+    """Handle the game joined event."""
+    context = cast(GameJoinedContext, message.context)
+    log.info("Joined game %s", context.game_id)
+
+
+def _handle_game_advertised(_player_id: str, message: Message) -> None:
+    """Handle the game advertised event."""
+    context = cast(GameAdvertisedContext, message.context)
+    log.info("Advertised game '%s': %s", context.game.name, context.game.game_id)
+
+
+def _handle_game_started(_player_id: str, message: Message) -> None:
+    """Handle the game started event."""
+    context = cast(GameStartedContext, message.context)
+    log.info("Started game: %s", context.game_id)
+
+
+def _handle_game_completed(_player_id: str, message: Message) -> None:
+    """Handle the game completed event."""
+    context = cast(GameCompletedContext, message.context)
+    log.info("Game completed: %s", context.comment if context.comment else "")
+
+
+def _handle_game_state_change(_player_id: str, message: Message) -> None:
+    "Handle the game state change event"
+    context = cast(GameStateChangeContext, message.context)
+    comment: Optional[str] = None
+    if context.recent_history:
+        history = context.recent_history[-1]
+        color = "General" if not history.color else history.color.value
+        action = history.action
+        comment = "%s - %s" % (color, action)
+    log.info("%s", comment if comment else "")
+
+
+def _handle_game_player_change(_player_id: str, message: Message) -> None:
+    """Handle the game player change event."""
+    context = cast(GamePlayerChangeContext, message.context)
+    players = [player.handle for player in context.players]
+    log.info("Game players are: %s", players)
+
+
+def _handle_game_player_turn(_player_id: str, message: Message) -> Optional[Message]:
+    """Handle the game player turn event."""
+    context = cast(GamePlayerTurnContext, message.context)
+    log.info("Turn for %s, %d moves available", context.handle, len(context.moves))
     return None
+
+
+def _handle_message(player_id: str, message: Message) -> Optional[Message]:
+    """Handle any message received from the connection."""
+    response = None
+    if message.message == MessageType.GAME_JOINED:
+        _handle_game_joined(player_id, message)
+    elif message.message == MessageType.GAME_ADVERTISED:
+        _handle_game_advertised(player_id, message)
+    elif message.message == MessageType.GAME_STARTED:
+        _handle_game_started(player_id, message)
+    elif message.message == MessageType.GAME_COMPLETED:
+        _handle_game_completed(player_id, message)
+    elif message.message == MessageType.GAME_STATE_CHANGE:
+        _handle_game_state_change(player_id, message)
+    elif message.message == MessageType.GAME_PLAYER_CHANGE:
+        _handle_game_player_change(player_id, message)
+    elif message.message == MessageType.GAME_PLAYER_TURN:
+        response = _handle_game_player_turn(player_id, message)
+    else:
+        log.info("Ignored message with type %s", message.message.name)
+    return response
+
 
 async def _handle_connection(websocket: WebSocketClientProtocol) -> None:
     """Handle a websocket connection, sending and receiving messages."""
@@ -65,7 +132,7 @@ async def _handle_connection(websocket: WebSocketClientProtocol) -> None:
     while True:
         message = await receive(websocket, timeout_sec=30)
         if message:
-            response = _handle_message(websocket, player_id, message)
+            response = _handle_message(player_id, message)
             if response:
                 await send(websocket, response)
 
