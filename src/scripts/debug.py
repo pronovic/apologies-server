5a8ae992935901276da574682d2461313211efbd
Kenneth Pronovici <ken.pronovici@objectpartners.com>
2020-05-14 17:26:14 -0500

    Add script so we can run in IntelliJ

---
+++ src/scripts/debug.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: set ft=python ts=4 sw=4 expandtab:
+# Start the websockets server, via a Python script that can be easily run/debugged from IntelliJ
+
+from apologiesserver.cli import cli
+
+cli("run_server")
